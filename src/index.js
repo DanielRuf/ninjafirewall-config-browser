d2991710a8f76078363a3707746da8f3778a0fbf
Daniel Ruf <mac1@daniel-ruf.de>
2021-02-20 21:39:19 +0100

    Implement logic for config selection - #4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const FileSaver = require('file-saver');
-const date = '20200910';
-const filename = `nfwp.4.2.4_${date}.dat`;
-const configData = `###CONFIG###
+const date = '20201214';
+const version = '4.3';
+const filename = `nfwp.${version}_${date}`;
+const configData = {
+'default': `###CONFIG###
 :-:
 ###RULES###
 :-:
-`;
+`,
+// 'custom': `###CONFIG_CUSTOM###
+// :-:
+// ###RULES###
+// :-:
+// `,
+};
+
+let currentConfig = 'default';
 require('./main.css');
 
 function init() {
@@ -20,12 +30,60 @@ function init() {
     }
 }
 
+function renderDropdown() {
+    const parent = document.querySelector('main');
+    const options = Object.keys(configData);
+    if (options.length && options.length === 1) {
+        return;
+    }
+
+    const selectListWrapper = document.createElement('div');
+    selectListWrapper.className = 'text-center';
+
+    const selectListWrapperInner = document.createElement('div');
+    selectListWrapperInner.innerText = 'Select configuration:';
+    selectListWrapperInner.className = 'py-2';
+
+    const selectList = document.createElement('select');
+    selectList.id = 'config-select';
+    selectList.className = 'bg-blue-500 text-white appearance-none border-none block rounded px-4';
+
+    parent.appendChild(selectListWrapper);
+    selectListWrapper.appendChild(selectListWrapperInner);
+    selectListWrapper.appendChild(selectList);
+
+    for (let i = 0; i < options.length; i++) {
+        var option = document.createElement('option');
+        option.value = options[i];
+        option.text = options[i];
+        if (i === 0) {
+            option.selected = 'selected';
+        }
+        selectList.appendChild(option);
+    }
+}
+
+function handleChangeConfig() {
+    currentConfig = this.value;
+    console.log(currentConfig);
+}
+
 function handleDownload(evt) {
     evt.preventDefault();
     const email = document.querySelector('[name="email"]').value;
-    const blob = new Blob([configData.replace('notifications@example.com', email)], {type: "text/plain;charset=utf-8"});
-    FileSaver.saveAs(blob, filename);
+    const blob = new Blob([configData[currentConfig].replace('notifications@example.com', email)], {type: "text/plain;charset=utf-8"});
+    if (currentConfig === 'default') {
+        FileSaver.saveAs(blob, `${filename}.dat`);
+    } else {
+        FileSaver.saveAs(blob, `${filename}_${currentConfig}.dat`);
+    }
 }
 
 init();
+renderDropdown();
+
+const selectElement = document.getElementById('config-select');
+if (selectElement) {
+    selectElement.addEventListener('change', handleChangeConfig, false);;
+}
 document.getElementById('download').addEventListener('submit', handleDownload, false);
